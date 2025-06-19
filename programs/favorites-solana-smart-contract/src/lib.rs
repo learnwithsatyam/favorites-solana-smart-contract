49234fe03ac815ccdad32ded9a6fffb7e43ad491
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-19 06:58:58 +0530

    solana contract commit 1

--- programs/favorites-solana-smart-contract/src/lib.rs
+++ programs/favorites-solana-smart-contract/src/lib.rs
@@ -1,6 +1,10 @@
 use anchor_lang::prelude::*;
 
-declare_id!("2G7wBpPfDCbePwygNkvBVHXK3nkaAje2rKxN2SU9obEp");
+// this is program address or id for out smart contract
+declare_id!("9bLJCC7xD9oyF4CozMBN3zgPhydqE5BzwqPxVaBZ1AEv"); // In solana playground, we dont need to fill this in. It will be automatically filled when we deploy our contract
+
+// It is something that writtent to every accound on blockchain by anchor program which just specifies the type of account it is. It is used by anchor for some of its checks
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8; // it means when we deploy on blockchain we will need 8 bytes + the size of whatever we are storing
 
 #[program]
 pub mod favorites_solana_smart_contract {
